acd7ff85bad5fb2d7cfca7b1b5b830407b50b1fd
UzairJ99 <uzair-jawaid@hotmail.com>
2021-08-17 14:35:10 -0400

    updated endpoint

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const authenticateToken = (req, res, next) => {
 }
 
 app.get('/', (_req, res) => {
-    res.send("hi.");
+    res.send("Hi from GCP kubernetes engine!");
 });
 
 app.get('/getPikachu', authenticateToken, async (_req, res) => {
